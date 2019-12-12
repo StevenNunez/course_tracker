4498599a251694ddc7f3a166db9a9ac2098e7cd3
Steven Nunez <steven.nunez@gmail.com>
2019-12-11 22:21:30 -0500

    Update query builder

--- app/models/course_query_builder.rb
+++ app/models/course_query_builder.rb
@@ -20,3 +20,26 @@ class CourseQueryBuilder
     Course.where(id: query.all).joins(:ratings).group(:id).having("avg(ratings.rating) > ?", rating)
   end
 end
+
+class CourseQueryBuilder
+  def run(params)
+    params.reduce(Course) do |query, (facet, constraint)|
+      case facet
+      when :tag
+        by_tag_list(query, constraint)
+      when :min_rating
+        by_min_rating(query, constraint)
+      else
+        query
+      end
+    end
+  end
+
+  def by_tag_list(query, tags)
+    Course.where(id: query.all).joins(:tags).where(tags: {name: tags}).group("id").having("COUNT(*) = ?", tags.count)
+  end
+
+  def by_min_rating(query, rating)
+    Course.where(id: query.all).joins(:ratings).group(:id).having("avg(ratings.rating) > ?", rating)
+  end
+end
